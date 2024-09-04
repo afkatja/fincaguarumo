370c36e642327f0975296918a53bde50e052849b
Katja Hollaar <afkatja@gmail.com>
2024-09-03 20:57:20 -0600

    add tour to metadata

--- fincaguarumo/sanity.config.ts
+++ fincaguarumo/sanity.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     }),
     documentInternationalization({
       supportedLanguages: i18n.languages,
-      schemaTypes: ["page", "post"],
+      schemaTypes: ["page", "post", "tour"],
       bulkPublish: true,
     }),
   ],
