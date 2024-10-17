e2d5a91c775bae23f5a53fafc7b4398e2453dd86
Katja Hollaar <afkatja@gmail.com>
2024-10-16 18:57:25 -0600

    fix build error

--- fincaguarumo/tailwind.config.ts
+++ fincaguarumo/tailwind.config.ts
@@ -59,20 +59,20 @@ const theme = {
         md: `calc(var(--radius) - 2px)`,
         sm: `calc(var(--radius) - 4px)`,
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: 0 },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: 0 },
+      //   },
+      // },
+      // animation: {
+      //   "accordion-down": "accordion-down 0.2s ease-out",
+      //   "accordion-up": "accordion-up 0.2s ease-out",
+      // },
       dropShadow: {
         sharp: "2px 2px 0 rgba(0,0,0, .8)",
         dots: "1px 1px 0 rgba(0,0,0, .5)",
