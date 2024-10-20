dbe0b64ec3d432a26269c35786193dc935557867
Katja Hollaar <afkatja@gmail.com>
2024-10-19 21:45:43 -0600

    tanager viewbox

--- fincaguarumo/src/components/icons/Tanager.tsx
+++ fincaguarumo/src/components/icons/Tanager.tsx
@@ -4,6 +4,7 @@ export default function Tanager(props: any) {
       xmlns="http://www.w3.org/2000/svg"
       width={props.size ?? 20}
       height={props.size ?? 20}
+      viewBox="0 0 1536 957"
       {...props}
     >
       <path
