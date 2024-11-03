0ff4903d00f30c85ad363f1ca4f04f6d3f670431
Katja Hollaar <afkatja@gmail.com>
2024-11-03 16:19:28 -0600

    add missing file

---
+++ fincaguarumo/src/sanity/schemaTypes/richTextType.ts
@@ -0,0 +1,20 @@
+import { defineType } from "sanity"
+import { breakType } from "./breakType"
+import RichText from "../../components/RichText"
+
+export const richTextType = defineType({
+  name: "breaks",
+  type: "object",
+  title: "Breaks",
+  components: {
+    input: RichText,
+  },
+  fields: [
+    {
+      name: "breakTypes",
+      type: "array",
+      title: "Break types",
+      of: [breakType],
+    },
+  ],
+})
