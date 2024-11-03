e89307d86d083d162ac0ec9ef106192f360f1e78
Katja Hollaar <afkatja@gmail.com>
2024-11-03 16:33:05 -0600

    remove breaks and rich text types

--- fincaguarumo/src/sanity/schema.ts
+++ fincaguarumo/src/sanity/schema.ts
@@ -8,7 +8,8 @@ import { pageType } from "./schemaTypes/pageType"
 import { tourType } from "./schemaTypes/tourType"
 import { homeType } from "./schemaTypes/homeType"
 import { dialogType } from "./schemaTypes/dialogType"
-import { richTextType } from "./schemaTypes/richTextType"
+// import { breakType } from "./schemaTypes/breakType"
+// import { richTextType } from "./schemaTypes/richTextType"
 import { galleryType } from "./schemaTypes/galleryType"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
@@ -21,7 +22,8 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     pageType,
     tourType,
     dialogType,
-    richTextType,
+    // breakType,
+    // richTextType,
     galleryType,
   ],
 }
