7e9c66877e083ca100b3c36d1e0cae2b65975343
Katja Hollaar <afkatja@gmail.com>
2024-11-03 19:07:25 -0600

    remove yet unused

--- fincaguarumo/src/sanity/schemaTypes/richTextType.ts
+++ fincaguarumo/src/sanity/schemaTypes/richTextType.ts
@@ -1,20 +1,21 @@
-import { defineType } from "sanity"
-import { breakType } from "./breakType"
-import RichText from "../../components/RichText"
+// import { defineType } from "sanity"
+// import { breakType } from "./breakType"
+// import RichText from "../../components/RichText"
 
-export const richTextType = defineType({
-  name: "breaks",
-  type: "object",
-  title: "Breaks",
-  components: {
-    input: RichText,
-  },
-  fields: [
-    {
-      name: "breakTypes",
-      type: "array",
-      title: "Break types",
-      of: [breakType],
-    },
-  ],
-})
+export const richTextType = {}
+// defineType({
+// name: "breaks",
+// type: "object",
+// title: "Breaks",
+// components: {
+//   input: RichText,
+// },
+// fields: [
+//   {
+//     name: "breakTypes",
+//     type: "array",
+//     title: "Break types",
+//     of: [breakType],
+//   },
+// ],
+// })
