dc5a217e86cf38e16edde146023f92dcb4f721f0
Katja Hollaar <afkatja@gmail.com>
2024-11-21 19:29:32 -0600

    nav dark mode text color

--- fincaguarumo/src/components/NavItem.tsx
+++ fincaguarumo/src/components/NavItem.tsx
@@ -9,10 +9,10 @@ const NavItem = ({ navItem }: { navItem: { href: string; title: string } }) => {
   return (
     <Link
       href={`/${navItem.href}`}
-      className={`lowercase text-sm fancy-underline hover:text-guarumo-accent dark:hover:text-white ${
+      className={`lowercase text-sm fancy-underline hover:text-guarumo-accent dark:hover:text-zinc-50 ${
         pathname.includes(navItem.href)
           ? "font-bold text-guarumo-accent active"
-          : "text-guarumo-primary dark:text-white font-medium"
+          : "text-guarumo-primary dark:text-zinc-50 font-medium"
       }`}
       prefetch={true}
     >
