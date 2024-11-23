b36037784a98afcac0b68e581a9c099efa4e5494
Katja Hollaar <afkatja@gmail.com>
2024-11-23 13:46:52 -0600

    fix build error

--- fincaguarumo/src/app/[locale]/(pages)/contact/page.tsx
+++ fincaguarumo/src/app/[locale]/(pages)/contact/page.tsx
@@ -1,6 +1,6 @@
 import ContactPage from "./ContactPage"
 
-const Contact = async ({ params }: { params: Record<string, string> }) => {
+const Contact = async ({ params }: { params: any }) => {
   const { locale } = await params
   return <ContactPage locale={locale} />
 }
