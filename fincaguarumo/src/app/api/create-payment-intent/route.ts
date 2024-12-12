ba2a4de5faa9eef30f32dfb2b78586ccc7d1a18b
Katja Hollaar <afkatja@gmail.com>
2024-12-12 09:46:04 -0600

    trying to get customer details for success page

--- fincaguarumo/src/app/api/create-payment-intent/route.ts
+++ fincaguarumo/src/app/api/create-payment-intent/route.ts
@@ -1,12 +1,22 @@
 import Stripe from "stripe"
 
 export async function POST(request: Request, response: Response) {
-  const { price } = await request.json()
+  const { price, description, fields } = await request.json()
 
   const stripeInstance = new Stripe(process.env.STRIPE_API_KEY ?? "")
+
+  const customer = await stripeInstance.customers.create({
+    name: fields.name,
+    email: fields.email,
+  })
   const paymentIntent = await stripeInstance.paymentIntents.create({
     amount: price * 100,
     currency: "usd",
+    description,
+    metadata: fields,
+    customer: customer.id,
+    setup_future_usage: "off_session",
+    receipt_email: fields.email,
     automatic_payment_methods: {
       enabled: true,
       // allow_redirects: 'never'
@@ -16,8 +26,15 @@ export async function POST(request: Request, response: Response) {
     // payment_method: paymentMethod,
   })
 
+  // const checkoutSession = await stripeInstance.checkout.sessions.create({
+  //   mode: "payment",
+  //   metadata: fields,
+  //   success_url: "http://localhost:3000/payment-success",
+  // })
+
   return Response.json({
     clientSecret: paymentIntent.client_secret,
+    // metadata: checkoutSession.metadata,
     // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
     dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
   })
