95f90ca77de35f3c5d3629f8699aff3fcaf432a4
Katja Hollaar <afkatja@gmail.com>
2025-04-18 17:57:23 -0600

    internal link for page query

--- fincaguarumo/src/sanity/lib/queries.ts
+++ fincaguarumo/src/sanity/lib/queries.ts
@@ -59,7 +59,16 @@ export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]{
 
 export const PAGE_QUERY = groq`
   *[_type == 'page' && slug.current == $pageName && language == $language][0] {
-    title, subtitle, description, mainImage, body, language, isPublished,
+    title, subtitle, description, mainImage, body {
+      ..., {
+        markDefs[] {
+        ...,
+        _type == "internalLink" => {
+          ...,
+          "slug": @.reference-> slug
+        }
+      }},
+    }, language, isPublished,
     "translations": *[
       _type == "translation.metadata" && 
       ^._id in translations[].value._ref
