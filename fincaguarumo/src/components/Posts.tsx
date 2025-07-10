d7ecbcad47a0321e5d8bc71b17683c9a83fcd581
Katja Hollaar <afkatja@gmail.com>
2025-07-10 16:36:38 -0600

    fix: link to posts with locale

--- fincaguarumo/src/components/Posts.tsx
+++ fincaguarumo/src/components/Posts.tsx
@@ -4,14 +4,20 @@ import Image from "next/image"
 import { POSTS_QUERYResult } from "../../sanity.types"
 import { urlFor } from "../sanity/lib/image"
 
-export function Posts({ posts }: { posts: POSTS_QUERYResult }) {
+export function Posts({
+  posts,
+  locale,
+}: {
+  posts: POSTS_QUERYResult
+  locale: string
+}) {
   return (
     <ul className="flex gap-2 w-11/12 mx-auto flex-wrap justify-center">
       {posts.map(post => (
         <li key={post._id} className="md:m-5 my-5 flex-initial md:w-56 w-full">
           <Link
             className="group no-underline flex flex-col items-center justify-content"
-            href={`/blog/${post?.slug?.current}`}
+            href={`${locale}/blog/${post?.slug?.current}`}
           >
             {post?.mainImage && (
               <Image
