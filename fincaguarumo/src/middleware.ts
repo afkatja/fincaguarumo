c3b0c769e6397c95cc25acf9a861ae624c00a868
Katja Hollaar <afkatja@gmail.com>
2025-07-10 16:50:51 -0600

    fix: add redirects for google indexing

--- fincaguarumo/src/middleware.ts
+++ fincaguarumo/src/middleware.ts
@@ -4,15 +4,13 @@ import { locales } from "./config"
 export default createMiddleware({
   locales,
   defaultLocale: "en",
-  // localePrefix: "never",
+  localePrefix: "always",
 })
 
 export const config = {
   // Match only internationalized pathnames
   matcher: [
     "/",
-    // "/(nl|en|es|ru)/:path*",
-    // "/((?!_next).*)",
-    "/((?!api|_next/static|_next/image|images|studio|favicon|assets|robots).*)",
+    "/((?!api|_next/static|_next/image|images|studio|favicon|assets|robots|_redirects).*)",
   ],
 }
