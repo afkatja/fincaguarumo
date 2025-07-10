9a13c0f9c43dfebf72d88621dd8d689e93dc7d94
Katja Hollaar <afkatja@gmail.com>
2025-07-10 16:50:29 -0600

    fix: add localized urls to sitemap

--- fincaguarumo/src/lib/generateSiteMap.ts
+++ fincaguarumo/src/lib/generateSiteMap.ts
@@ -5,52 +5,61 @@ import {
   ALL_PAGES_QUERY,
   POSTS_QUERY,
   TOURS_QUERY,
-  GALLERY_QUERY,
 } from "../sanity/lib/queries"
 
 const fetchContent = async (): Promise<{
   posts: SanityDocument[]
   tours: SanityDocument[]
   pages: SanityDocument[]
-  gallery: SanityDocument[]
 }> => {
-  const [posts, tours, pages, gallery] = await Promise.all([
+  const [posts, tours, pages] = await Promise.all([
     sanityFetch({ query: POSTS_QUERY }) as Promise<SanityDocument[]>,
     sanityFetch({ query: TOURS_QUERY, params: { language: "en" } }) as Promise<
       SanityDocument[]
     >,
     sanityFetch({ query: ALL_PAGES_QUERY }) as Promise<SanityDocument[]>,
-    sanityFetch({ query: GALLERY_QUERY }) as Promise<SanityDocument[]>,
   ])
 
-  return { posts, tours, pages, gallery }
+  return { posts, tours, pages }
 }
 
-const generateUrl = (type: string, item: SanityDocument) => {
-  return {
-    url: `/${type}/${item.slug.current}`,
+const locales = ["en", "es", "ru", "nl", "de"]
+const generateLocalizedUrls = (type: string, item: SanityDocument) => {
+  return locales.map(locale => ({
+    url: `/${locale}/${type}/${item.slug.current}`,
     lastmod: new Date(item._updatedAt).toISOString(),
     changefreq: "weekly",
     priority: 0.7,
-  }
+  }))
 }
 
 export const generateSitemap = async () => {
-  const { posts, tours, pages, gallery } = await fetchContent()
+  const { posts, tours, pages } = await fetchContent()
 
   // Add main gallery page
-  const mainGalleryUrl = {
-    url: "/gallery",
-    lastmod: new Date().toISOString(),
-    changefreq: "weekly",
-    priority: 0.7,
+  const mainGalleryUrl = () => {
+    return locales.map(locale => ({
+      url: `${locale}/gallery`,
+      lastmod: new Date().toISOString(),
+      changefreq: "weekly",
+      priority: 0.7,
+    }))
   }
 
+  const mainPages = () => {
+    return locales.map(locale => ({
+      url: `/${locale}`,
+      lastmod: new Date().toISOString(),
+      changefreq: "daily",
+      priority: 1.0,
+    }))
+  }
   const urls = [
-    ...posts.map(post => generateUrl("blog", post)),
-    ...tours.map(tour => generateUrl("tours", tour)),
-    ...pages.map(page => generateUrl("pages", page)),
-    mainGalleryUrl,
+    ...mainPages(),
+    ...posts.flatMap(post => generateLocalizedUrls("blog", post)),
+    ...tours.flatMap(tour => generateLocalizedUrls("tours", tour)),
+    ...pages.flatMap(page => generateLocalizedUrls("pages", page)),
+    ...mainGalleryUrl(),
   ]
 
   const sitemap = xml({
