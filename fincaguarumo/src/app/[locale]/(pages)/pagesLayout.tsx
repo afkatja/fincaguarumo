35d5ef0e896b2bc416e60adadf7bbbdd75f40244
Katja Hollaar <afkatja@gmail.com>
2025-09-26 11:15:24 -0600

    chore: preload assets

--- fincaguarumo/src/app/[locale]/(pages)/pagesLayout.tsx
+++ fincaguarumo/src/app/[locale]/(pages)/pagesLayout.tsx
@@ -7,6 +7,7 @@ import RichText from "@/components/RichText"
 import Slideshow from "@/components/Slideshow"
 import { shuffle } from "@/lib/utils"
 import { SanityImageObject } from "../../../types"
+import Head from "next/head"
 
 const PageLayout = ({
   pageName,
@@ -30,50 +31,60 @@ const PageLayout = ({
 }) => {
   if (!pageName) notFound()
   return (
-    <Suspense fallback={<Loading className="absolute" />}>
-      <div className="bg-zinc-50 dark:bg-zinc-900 pt-5 lg:pt-8 content-wrap z-10 flex-1">
-        <div className="prose w-11/12 lg:prose-lg mx-auto relative z-20">
-          <Title
-            titleClassName="text-5xl font-bold mb-5 lg:mb-8 text-guarumo-accent dark:text-zinc-50"
-            icon={{
-              iconClassName: "fill-guarumo-accent dark:fill-zinc-50",
-            }}
-            title={title}
-            Heading="h1"
-          />
-          {subtitle && (
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://cdn.sanity.io"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Suspense fallback={<Loading className="absolute" />}>
+        <div className="bg-zinc-50 dark:bg-zinc-900 pt-5 lg:pt-8 content-wrap z-10 flex-1">
+          <div className="prose w-11/12 lg:prose-lg mx-auto relative z-20">
             <Title
-              titleClassName="text-3xl font-bold my-3 text-guarumo-primary dark:text-zinc-50"
-              title={subtitle}
-              Heading="h2"
-            />
-          )}
-        </div>
-        {(mainImage || images) && (
-          <Slideshow
-            images={
-              images
-                ? (shuffle(images) as unknown as SanityImageObject[])
-                : [mainImage]
-            }
-          />
-        )}
-        {description && (
-          <section className="!w-11/12 !pt-6 lg:py-2 prose lg:prose-lg mx-auto">
-            <Title
-              titleClassName="text-2xl font-bold text-guarumo-primary dark:text-zinc-50"
-              title={description}
-              Heading="h3"
+              titleClassName="text-5xl font-bold mb-5 lg:mb-8 text-guarumo-accent dark:text-zinc-50"
               icon={{
                 iconClassName: "fill-guarumo-accent dark:fill-zinc-50",
               }}
+              title={title}
+              Heading="h1"
             />
-          </section>
-        )}
-        {body && <RichText body={body} icon={iconProp} />}
-        {children}
-      </div>
-    </Suspense>
+            {subtitle && (
+              <Title
+                titleClassName="text-3xl font-bold my-3 text-guarumo-primary dark:text-zinc-50"
+                title={subtitle}
+                Heading="h2"
+              />
+            )}
+          </div>
+          {(mainImage || images) && (
+            <Slideshow
+              images={
+                images
+                  ? (shuffle(images) as unknown as SanityImageObject[])
+                  : [mainImage]
+              }
+            />
+          )}
+          {description && (
+            <section className="!w-11/12 !pt-6 lg:py-2 prose lg:prose-lg mx-auto">
+              <Title
+                titleClassName="text-2xl font-bold text-guarumo-primary dark:text-zinc-50"
+                title={description}
+                Heading="h3"
+                icon={{
+                  iconClassName: "fill-guarumo-accent dark:fill-zinc-50",
+                }}
+              />
+            </section>
+          )}
+          {body && <RichText body={body} icon={iconProp} />}
+          {children}
+        </div>
+      </Suspense>
+    </>
   )
 }
 
