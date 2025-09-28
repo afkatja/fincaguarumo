177b43391e44fd3de1e23ce7a250daf1572dcc2d
Katja Hollaar <afkatja@gmail.com>
2025-09-27 21:27:43 -0600

    chore: lang selector glyphs styling

--- fincaguarumo/src/components/LanguageSelector.tsx
+++ fincaguarumo/src/components/LanguageSelector.tsx
@@ -45,9 +45,9 @@ const LanguageSelector = ({
             <div className="flex gap-2 items-center">
               <Icon
                 icon={version.glyph}
-                size={20}
+                size={26}
                 color="#9d1f60"
-                className="fill-guarumo-accent dark:fill-zinc-50"
+                className="fill-guarumo-accent dark:fill-zinc-50 rounded-full border-zinc-50 border p-1"
               />
               <span className="hidden lg:block">{version.title}</span>
             </div>
