4540f961597c6ed283310dee3cb8100e426c1e1d
Katja Hollaar <afkatja@gmail.com>
2025-11-17 11:41:49 -0600

    feat: add airbnb/booking reviews

---
+++ fincaguarumo/data/booking-reviews.ts
@@ -0,0 +1,13 @@
+const reviews = [
+  {
+    platform: "booking",
+    author: { name: "Henri", location: "Netherlands" },
+    date: "July 8, 2025",
+    rating: 9,
+    reviewText: `Great value for the money!
+We were very satisfied with our visit. The house was off great quality and clean. We had everything we needed. Even though the house is located in the jungle, there was always hot and cold water. The beds were nice and comfortable. We enjoyed our stay.
+Thanks Peter and Katia.
+Be aware since this is a jungle house there is no air conditioner.`,
+  },
+]
+export default reviews
