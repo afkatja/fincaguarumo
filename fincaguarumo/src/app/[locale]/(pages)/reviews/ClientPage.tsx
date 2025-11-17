e1ef1f3b77fbda8d3b19276890593a83f77cb9eb
Katja Hollaar <afkatja@gmail.com>
2025-11-17 11:45:58 -0600

    feat: add reviews page

---
+++ fincaguarumo/src/app/[locale]/(pages)/reviews/ClientPage.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from "react"
+
+import { APIProvider } from "@vis.gl/react-google-maps"
+import { PlaceProvider } from "../../../providers/PlaceProvider"
+import { PlaceReviews } from "../../../../components/PlaceReviews"
+import { placeId } from "../../../../../data/geo"
+
+const ClientPage = () => {
+  return (
+    <APIProvider
+      apiKey={process.env.NEXT_PUBLIC_GMAPS_API_KEY as string}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <PlaceProvider placeId={placeId}>
+        <PlaceReviews />
+      </PlaceProvider>
+    </APIProvider>
+  )
+}
+
+export default ClientPage
