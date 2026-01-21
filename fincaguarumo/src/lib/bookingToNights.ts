95c928fd204e5dd5a409fc9e3f7a0960bcde2150
Katja Hollaar <afkatja@gmail.com>
2026-01-21 08:15:47 -0600

    fix: adjust calculation of imported calendars

--- fincaguarumo/src/lib/bookingToNights.ts
+++ fincaguarumo/src/lib/bookingToNights.ts
@@ -1,15 +1,14 @@
 export default function bookingToNights(start: Date, end: Date) {
   const nights: Date[] = []
 
-  // Start from the day after check-in (check-out/check-in day)
+  // Start from the day after check-in
   const cur = new Date(start)
-  cur.setUTCDate(cur.getUTCDate())
+  cur.setUTCDate(cur.getUTCDate() + 1)
 
-  // End at the day before check-out (check-out/check-in day)
+  // End at the day of check-out
   const checkoutExclusive = new Date(end)
-  checkoutExclusive.setUTCDate(checkoutExclusive.getUTCDate() - 1)
 
-  while (cur <= checkoutExclusive) {
+  while (cur < checkoutExclusive) {
     nights.push(new Date(cur))
     cur.setUTCDate(cur.getUTCDate() + 1)
   }
