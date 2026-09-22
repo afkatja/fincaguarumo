7c2f9179affe475c3db4b6c0160c8cee2baccfe6
Katja Hollaar <afkatja@gmail.com>
2026-09-22 10:24:10 -0600

    chore: update typescript eslint config

--- fincaguarumo/eslint.config.mjs
+++ fincaguarumo/eslint.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from "eslint/config"
 import js from "@eslint/js"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     ignores: ["./next/*", "node_modules"],
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         browser: true,
         es2021: true,
@@ -16,7 +26,8 @@ export default defineConfig([
       },
     },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "off",
       "no-undef": "off",
     },
